f10d33b52dda905c51e0d3054f3415795f982dbc
Edgar Valderrama <valderrama311@gmail.com>
2022-07-15 03:23:00 -0500

    fix: level

--- Fetch Game/fetch/game/scripting/draw_bones_action.py
+++ Fetch Game/fetch/game/scripting/draw_bones_action.py
@@ -7,7 +7,7 @@ class DrawBonesAction(Action):
 
     def __init__(self, video_service):
         self._video_service = video_service
-        # self._bones_shown = []
+        self._bones_shown = []
         self._counter = 0
 
     def execute(self, cast, script, callback):
@@ -28,17 +28,21 @@ class DrawBonesAction(Action):
             order = bone.get_order()
             # velocity = body.get_velocity()
 
-            if position.get_x() == 10:
-                # if order not in self._bones_shown:
-                    # self._bones_shown.append(order)
+            if position.get_x() >= 100 and position.get_x() <= 150:
+                if order not in self._bones_shown:
+                    self._bones_shown.append(order)
 
-                self._counter += 1
+                    self._counter += 1
 
-                print("*--------")
-                print(self._counter)
-                print()
+                    print("*--------")
+                    print(self._counter)
+                    print()
 
-                if self._counter == 100:
-                    stats = cast.get_first_actor(STATS_GROUP)
-                    stats.next_level()
-                    self._counter = 0
+                    if self._counter == 100:
+                        stats = cast.get_first_actor(STATS_GROUP)
+                        stats.next_level()
+                        self._counter = 0
+
+            if position.get_x() == 1280:
+                if order in self._bones_shown:
+                    self._bones_shown.remove(order)
