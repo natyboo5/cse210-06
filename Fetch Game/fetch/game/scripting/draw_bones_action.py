1653999498345411ed29846c7f86a9624fd82223
Edgar Valderrama <valderrama311@gmail.com>
2022-07-18 17:44:34 -0500

    Change background when level changes

--- Fetch Game/fetch/game/scripting/draw_bones_action.py
+++ Fetch Game/fetch/game/scripting/draw_bones_action.py
@@ -2,6 +2,10 @@
 from constants import *
 from game.scripting.action import Action
 from game.casting.sound import Sound
+from game.casting.body import Body
+from game.casting.image import Image
+from game.casting.point import Point
+from game.casting.background import Background
 
 
 class DrawBonesAction(Action):
@@ -11,6 +15,7 @@ class DrawBonesAction(Action):
         self._video_service = video_service
         self._bones_shown = []
         self._counter = 0
+        self._bg_counter = 1
 
     def execute(self, cast, script, callback):
         bones = cast.get_actors(BONE_GROUP)
@@ -45,11 +50,28 @@ class DrawBonesAction(Action):
                         self._audio_service.play_sound(sound)
                         sound = Sound(GAME_SOUND)
                         self._audio_service.play_sound(sound)
-                        
+
                         stats = cast.get_first_actor(STATS_GROUP)
                         stats.next_level()
                         self._counter = 0
 
+                        self._bg_counter += 1
+                        self._add_background(cast, self._bg_counter)
+
             if position.get_x() == 1280:
                 if order in self._bones_shown:
                     self._bones_shown.remove(order)
+
+    def _add_background(self, cast, image_number):
+        cast.clear_actors(BACKGROUND_GROUP)
+        x = 0
+        y = 0
+        position = Point(x, y)
+        size = Point(SCREEN_WIDTH, SCREEN_HEIGHT)
+        body = Body(position, size)
+        if image_number > 3:
+            image_number = 3
+        image_path = globals()[f"BACKGROUND_LEVEL{image_number}"]
+        image = Image(image_path)
+        background = Background(body, image, True)
+        cast.add_actor(BACKGROUND_GROUP, background)
